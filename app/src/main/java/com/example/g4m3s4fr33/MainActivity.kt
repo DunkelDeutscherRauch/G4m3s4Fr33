e1ad079247d651b11a8562016ddf7e7c0efcad75
Eike Matthias Engelmann <SError@gmx.de>
2024-04-12 12:36:43 +0200

    update Friday 2024-04-12 12:36 pm

--- app/src/main/java/com/example/g4m3s4fr33/MainActivity.kt
+++ app/src/main/java/com/example/g4m3s4fr33/MainActivity.kt
@@ -1,13 +1,31 @@
 package com.example.g4m3s4fr33
 
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import com.example.g4m3s4fr33.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        handleOnBackPressed()
+
+    }
+
+    private fun handleOnBackPressed() {
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                binding.fragmentContainerView.findNavController().navigateUp()
+            }
+        }
+        onBackPressedDispatcher.addCallback(callback)
     }
 }
