2f3ad76cdcf5ae8ac562f90a6fd7b64b4b85e6ad
Eike Matthias Engelmann <SError@gmx.de>
2024-04-22 15:45:09 +0200

    update Monday 2024-04-22 03:45 pm

--- app/src/main/java/com/example/g4m3s4fr33/ui/GameDetailFragment.kt
+++ app/src/main/java/com/example/g4m3s4fr33/ui/GameDetailFragment.kt
@@ -7,9 +7,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.OvershootInterpolator
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import coil.load
+import com.example.g4m3s4fr33.R
 import com.example.g4m3s4fr33.WaifuViewModel
 import com.example.g4m3s4fr33.databinding.FragmentGameDetailBinding
 
@@ -32,13 +34,33 @@ class GameDetailFragment : Fragment() {
 
         val gameId = arguments?.getInt("gameId")
 
+        val expandableTextView = binding.expTVDescription
+        val toggle = binding.tvSeeMoreOrLess
+
         viewModel.getGameDetail(gameId!!)
+
+        expandableTextView.setAnimationDuration(750L)
+        expandableTextView.setInterpolator(OvershootInterpolator())
 
         viewModel.gameDetail.observe(viewLifecycleOwner) {
             binding.imageView.load(viewModel.gameDetail.value!!.thumbnail)
-            binding.textView4.text = viewModel.gameDetail.value!!.descriptionLong
-        }
+            binding.expTVDescription.text = viewModel.gameDetail.value!!.descriptionLong
 
+            binding.tvSeeMoreOrLess.setOnClickListener {
+
+                if (expandableTextView.isExpanded) {
+                    expandableTextView.collapse()
+                    toggle.setText(R.string.see_more)
+
+                } else {
+                    expandableTextView.expand()
+                    toggle.setText(R.string.see_less)
+                }
+            }
+
+
+
+        }
 
         binding.button.setOnClickListener {
             startActivity(
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ buildscript {
     }
     dependencies {
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
+
     }
 }
 
@@ -12,3 +13,4 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
 }
+
