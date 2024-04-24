5e945683ca962d824ffac2fd6d1061e5812e4b58
Eike Matthias Engelmann <SError@gmx.de>
2024-04-24 16:05:26 +0200

    update Wednesday 2024-04-24 04:05 pm

--- app/src/main/java/com/example/g4m3s4fr33/ui/FavoriteFragment.kt
+++ app/src/main/java/com/example/g4m3s4fr33/ui/FavoriteFragment.kt
@@ -5,18 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.g4m3s4fr33.databinding.FragmentFavoritesBinding
+import com.example.g4m3s4fr33.databinding.FragmentFavoriteBinding
+
 
 class FavoriteFragment : Fragment() {
 
-    private lateinit var binding: FragmentFavoritesBinding
+    private lateinit var binding: FragmentFavoriteBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentFavoritesBinding.inflate(layoutInflater, container, false)
+        binding = FragmentFavoriteBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
