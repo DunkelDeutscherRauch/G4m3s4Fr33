6f40e81affbe0e6816a9b888b7cda1794a0c3e09
Eike Matthias Engelmann <SError@gmx.de>
2024-05-13 18:21:14 +0200

    update Monday 2024-05-13 06:21 pm

--- app/src/main/java/com/example/g4m3s4fr33/my_crunch_time/ToDo.kt
+++ app/src/main/java/com/example/g4m3s4fr33/my_crunch_time/ToDo.kt
@@ -1,13 +1,13 @@
 package com.example.g4m3s4fr33.my_crunch_time
 
 /*
-TODO —> WaifuViewModel
+TODO —> ProfileFragment (alertDialog)
+TODO —> WaifuViewModel (ranks)
 TODO —> HomeFragment (layout)
 TODO —> GameListFragment (layout)
-TODO —> FavoriteDetailFragment
-TODO —> ProfileFragment
-TODO —> strings.xml
-TODO —> styles.xml
-TODO -> themes.xml
+TODO —> FavoriteDetailFragment (layout)
+TODO —> strings.xml (div. layouts)
+TODO —> styles.xml (main button style)
+TODO -> themes.xml (see above)
 TODO —> review all layouts
  */
