fb6354f74c4df30814c021a6562b2cb10afd235c
Eike Matthias Engelmann <SError@gmx.de>
2024-05-22 15:22:56 +0200

    update Wednesday 2024-05-22 03:22 pm

--- app/src/main/java/com/example/g4m3s4fr33/my_crunch_time/ToDo.kt
+++ app/src/main/java/com/example/g4m3s4fr33/my_crunch_time/ToDo.kt
@@ -1,12 +1,21 @@
 package com.example.g4m3s4fr33.my_crunch_time
 
-/*
-TODO —> WaifuViewModel (ranks)
-TODO —> HomeFragment (layout)
-TODO —> GameListFragment (layout)
-TODO —> FavoriteDetailFragment (layout)
-TODO —> strings.xml (div. layouts)
-TODO —> styles.xml (main button style)
-TODO -> themes.xml (see above)
-TODO —> review all layouts
+/**
+TODO
+ -> WaifuViewModel (ranks)
+ —> HomeFragment (layout)
+ —> GameListFragment (layout)
+ —> FavoriteDetailFragment (layout)
+ —> strings.xml (div. layouts)
+ —> styles.xml (main button style)
+ -> themes.xml (see above)
+ —> review all layouts
+*/
+
+/**
+TODO - cool stuff for the future (maybe) -
+ -> implement SteamAPI (not the easiest one -> see https://steamcommunity.com/dev?l=german)
+ -> autoComplete by name (complex)
+ -> keep position in recyclerView combined with handleOnBackPressed (complex)
+ -> autoMigration for RoomDB (https://developer.android.com/training/data-storage/room/migrating-db-versions?hl=de)
  */
