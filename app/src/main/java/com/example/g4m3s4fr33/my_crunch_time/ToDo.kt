be55b303527c563498d06d5ed72e0d85dd85586e
Eike Matthias Engelmann <SError@gmx.de>
2024-05-27 19:59:33 +0200

    update Monday 2024-05-27 07:58 pm

--- app/src/main/java/com/example/g4m3s4fr33/my_crunch_time/ToDo.kt
+++ app/src/main/java/com/example/g4m3s4fr33/my_crunch_time/ToDo.kt
@@ -7,7 +7,7 @@ TODO
  -> FavoriteListItem (layout migrate vom test_layout.xml to favorite_list_item.xml)
  —> strings.xml (div. layouts)
  —> review all layouts
- -> does it work?
+ -> does it?
 */
 
 /**
